244284b1d179d49ec659ab969d2d0b2658886cd4
VoiceOfYggdrasil <aaron.schmitt8@gmail.com>
2024-11-13 09:54:55 -0500

    Filled out Order Class & Added CheckoutScreen

--- Sub-41/src/main/java/com/pluralsight/program/Order.java
+++ Sub-41/src/main/java/com/pluralsight/program/Order.java
@@ -1,4 +1,45 @@
 package com.pluralsight.program;
 
+import com.pluralsight.addOns.Chips;
+import com.pluralsight.addOns.Drinks;
+import com.pluralsight.sandwich.Sandwich;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private List<Sandwich> sandwiches = new ArrayList<>();
+    private List<Drinks> drinks = new ArrayList<>();
+    private List<Chips> chips = new ArrayList<>();
+
+    public void addSandwich(Sandwich sandwich) {
+        sandwiches.add(sandwich);
+    }
+
+    public void addDrink(Drinks drink) {
+        drinks.add(drink);
+    }
+
+    public void addChips(Chips chip) {
+        chips.add(chip);
+    }
+
+    public double getTotalPrice() {
+        double total = 0;
+
+        for (Sandwich sandwich : sandwiches) {
+            total += sandwich.getPrice();
+        }
+        for (Drinks drink : drinks) {
+            total += drink.getPrice();
+        }
+        for (Chips chip : chips) {
+            total += chip.getPrice();
+        }
+        return total;
+    }
+
+    public void checkOut(){
+
+    }
 }
