6c54a5749b5aaf70a27284d57bfd224265892c16
VoiceOfYggdrasil <aaron.schmitt8@gmail.com>
2024-11-14 10:39:47 -0500

    Overhauling Menu UI

--- Sub-41/src/main/java/com/pluralsight/UI/UserInterface.java
+++ Sub-41/src/main/java/com/pluralsight/UI/UserInterface.java
@@ -2,31 +2,88 @@ package com.pluralsight.UI;
 
 import com.pluralsight.program.Order;
 
+import java.util.Scanner;
+
 public class UserInterface {
-    private Order thisOrder;
-    private MainScreen mainScreen;
+    private Order currentOrder;
+    private Scanner scanner = new Scanner(System.in);
 
     public UserInterface() {
-        mainScreen = new MainScreen();
+        this.currentOrder = new Order();
+    }
+
+    public void startProgram() {
+        displayMainScreen();
     }
 
     public void displayMainScreen() {
-        mainScreen.displayMenu();
+        System.out.println("Welcome to SUB-41, house of good subs and bad puns! ");
+        System.out.println("\t1. New Order");
+        System.out.println("\t2. Exit");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        if (choice == 1) {
+            newOrder();
+        } else if (choice == 2) {
+            System.out.println("Thank you for stopping by!");
+        } else {
+            System.out.println("ERROR: Invalid Choice.");
+            displayMainScreen();
+        }
     }
 
     public void newOrder() {
+        this.currentOrder = new Order();
+        displayOrderScreen();
+    }
+
+    public void displayOrderScreen() {
 
     }
 
-    public void displayAllMenus() {
+    public void addSandwich() {
 
     }
 
-    public void orderSummary() {
+    public void chooseSandwichSize() {
 
     }
 
-    public void confirmOrder() {
+    public void chooseBread() {
 
+    }
+
+    public void chooseToast() {
+
+    }
+
+    public void chooseMeat() {
+
+    }
+
+    public void chooseCheese() {
+
+    }
+
+    public void chooseSauce() {
+
+    }
+
+    public void chooseRegTopping() {
+
+    }
+
+    public void chooseDrink() {
+
+    }
+
+    public void chooseChips() {
+
+    }
+
+    public void displayCheckoutScreen() {
+        
     }
 }
